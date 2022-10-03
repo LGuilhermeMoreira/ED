7d792335b50fcff62d9b3d9fa4a8b585bef63b2a
Guilhermemoreia <Lguilhermemoreiraleite@gmail.com>
2022-10-03 20:58:51 -0300

    update

---
+++ lista_linear/veto_consulta.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int matchString(vector<string> a, string b)
+{
+    int cont = 0;
+    for (int i = 0; i < a.size(); i++)
+    {
+        if (a[i] == b)
+        {
+            cont++;
+        }
+    }
+    // cout << "valor de cont: " << cont << endl;
+    return cont;
+}
+
+int main()
+{
+    int con, bus;
+    vector<int> vec;
+    cin >> con;
+    cin.ignore();
+
+    vector<string> consulta(con);
+
+    for (int i = 0; i < con; i++)
+    {
+        cin >> consulta[i];
+        cin.ignore();
+    }
+
+    cin >> bus;
+    vector<string> busca(bus);
+
+    for (int i = 0; i < bus; i++)
+    {
+        cin >> busca[i];
+        cin.ignore();
+    }
+
+    for (int i = 0; i < bus; i++)
+    {
+        vec.push_back(matchString(consulta, busca[i]));
+    }
+
+    for (int i = 0; i < vec.size(); i++)
+    {
+        if (i == vec.size() - 1)
+        {
+            cout << vec[i];
+            break;
+        }
+        cout << vec[i] << " ";
+    }
+    cout << endl;
+}
